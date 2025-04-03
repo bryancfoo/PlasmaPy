32e9caa0cf0d2ab88e18ce10baa76e7caf7e79df
bryancfoo <68606269+bryancfoo@users.noreply.github.com>
2025-04-03 11:39:15 -0400

    use nansum to normalize instead of sum

--- src/plasmapy/diagnostics/thomson.py
+++ src/plasmapy/diagnostics/thomson.py
@@ -1086,7 +1086,7 @@ def _spectral_density_model(wavelengths, settings=None, **params):
         notch=notch,
     )
 
-    model_Skw *= 1 / np.sum(model_Skw)
+    model_Skw *= 1 / np.nansum(model_Skw)
 
     # Add background after normalization
     model_Skw += background
@@ -1416,7 +1416,7 @@ def _spectral_density_supergaussian_model(wavelengths, settings=None, **params):
         instr_func_arr=instr_func_arr
     )
 
-    model_Skw *= 1 / np.sum(model_Skw)
+    model_Skw *= 1 / np.nansum(model_Skw)
 
     return model_Skw
 
