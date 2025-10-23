dad51105296bcb6a132c7191164813da301b9e6d
bryancfoo <68606269+bryancfoo@users.noreply.github.com>
2025-10-23 16:38:23 -0400

    Update thomson.py

--- src/plasmapy/diagnostics/thomson.py
+++ src/plasmapy/diagnostics/thomson.py
@@ -649,7 +649,8 @@ def spectral_density(  # noqa: C901, PLR0912, PLR0915
     else:
         ifract = np.asarray(ifract, dtype=np.float64)
         if np.sum(ifract) != 1:
-            raise ValueError(f"The provided ifract does not sum to 1: {ifract}")
+            ifract/=np.linalg.norm(ifract)
+            #raise ValueError(f"The provided ifract does not sum to 1: {ifract}")
 
     if probe_vec is None:
         probe_vec = np.array([1, 0, 0])
