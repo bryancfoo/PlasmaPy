34efb6e54d3135aa0b9bcd8347a970362d11ae7a
bryancfoo <68606269+bryancfoo@users.noreply.github.com>
2025-10-31 17:27:27 -0400

    Update thomson.py

--- src/plasmapy/diagnostics/thomson.py
+++ src/plasmapy/diagnostics/thomson.py
@@ -1093,10 +1093,6 @@ def _spectral_density_model(wavelengths, settings=None, **params):
     # Add background after normalization
     model_Skw += background
 
-
-
-
-
     return model_Skw
 
 
@@ -1422,6 +1418,7 @@ def _spectral_density_supergaussian_model(wavelengths, settings=None, **params):
         instr_func_arr=instr_func_arr
     )
 
+    model_Skw *= 1 / np.nanmax(model_Skw)
     model_Skw *= 1 / np.nanmax(model_Skw)
 
     return model_Skw
